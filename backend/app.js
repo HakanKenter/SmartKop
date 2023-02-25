04fe3e879fb329d9ae25077c9294c18d577b8c6e
HakanKenter <hakan.kt@outlook.fr>
2023-02-26 00:23:27 +0100

    local app version

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const dotenv = require('dotenv');
-const path = require('path');
 
 const errorMiddleware = require('./middlewares/errors');
 
@@ -37,14 +36,6 @@ app.use('/api/v1', products)
 app.use('/api/v1', auth)
 app.use('/api/v1', payment)
 app.use('/api/v1', order)
-
-if(process.env.NODE_ENV === 'PRODUCTION') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
-    })
-}
 
 /******************************************************************************/
 /****************************** Use middleswares ******************************/
