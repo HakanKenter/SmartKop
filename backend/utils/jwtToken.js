2c6288692645f5ae3e5073283d1b52668fb08d92
HakanKenter <hakan.kt@outlook.fr>
2023-02-26 03:36:35 +0100

    test

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -4,7 +4,7 @@ const sendToken = (user, statusCode, res) => {
     // Create Jwt token
     const token = user.getJwtToken();
 
-    // Options for cookie
+    // Options for cookie 
     const options = {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000 ),
         httpOnly: true
