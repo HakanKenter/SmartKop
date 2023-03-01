c4781a90eccb6bbb6fe365aebfa8594f46c854b9
HakanKenter <hakan.kt@outlook.fr>
2023-03-01 22:53:43 +0100

    changed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -119,6 +119,7 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
 
     // Create reset password url
     const resetUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
+    // const resetUrl = `${req.protocol}://${req.get('host')}/password/reset/${resetToken}`;    
 
     const html = `
     <h3>Demande de réinitialisation.</h3>
